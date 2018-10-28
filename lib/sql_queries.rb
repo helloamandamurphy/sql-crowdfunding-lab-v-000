271e2c5eda4e175ce5f0207747bba6cb350616f4
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 19:05:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ FROM projects
 LEFT OUTER JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.id
-HAVING SUM((pledges.amount)-projects.funding_goal) > 0"
+HAVING amount_left > 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
