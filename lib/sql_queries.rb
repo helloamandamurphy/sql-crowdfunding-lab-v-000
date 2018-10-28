5fce3f9b404d89eab18899005e188e577da9f542
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-28 19:11:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ FROM projects
 JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY projects.title
-HAVING amount_left > 0"
+HAVING amount_left >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
